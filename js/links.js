cf81a6c4790ecbf8be261dc17ede10bda3f9e09e
aymkh <91288221+MDS-ISALIB@users.noreply.github.com>
2021-09-24 14:26:02 +0100

    sitemap links

--- js/links.js
+++ js/links.js
@@ -1,4 +1,6 @@
 const linksArea = document.querySelector('#main-links')
+const linksSiteMap = document.querySelector('#sitemap-links')
+
 
 const links = [
     {'title': 'fb'},
@@ -15,5 +17,69 @@ const createLink= t => {
 }
 
 links.forEach(t=>{
-    linksArea.innerHTML += createLink(t)
+    if (linksArea) {
+        linksArea.innerHTML += createLink(t)
+    }
+})
+
+const sitemap = [
+    {
+        'title': 'bigdata',
+        'ab': 'Big Data'
+    },
+    {
+        'title': 'c',
+        'ab': 'Complexité Algorithmique'
+    },
+    {
+        'title': 'cpd',
+        'ab': 'Calcul Parallèle et Distribué'
+    },
+    {
+        'title': 'culture',
+        'ab': 'Culture d\'Entreprises'
+    },
+    {
+        'title': 'eng',
+        'ab': 'Anglais'
+    },
+    {
+        'title': 'fr',
+        'ab': 'Français'
+    },
+    {
+        'title': 'java',
+        'ab': 'Java'
+    },
+    {
+        'title': 'math',
+        'ab': 'Fondements Mathématiques des Données Scientifique'
+    },
+    {
+        'title': 'nosql',
+        'ab': 'NoSQL'
+    },
+    {
+        'title': 'plsql',
+        'ab': 'PL_SQL'
+    },
+    {
+        'title': 'r',
+        'ab': 'R Programming'
+    },
+    {
+        'title': 'sas',
+        'ab': 'Traitement analytique des Bases de Données'
+    }
+]
+const populateSiteMap = (t)=>{
+    return `
+        <ul>
+            <li style="margin-top: 1rem;"><a href="https://mds-islaib.tn/c/${t.title}" target="_blank"><strong>${t.ab}</strong> ** mds-islaib.tn/c/${t.title}/</a></li>
+        </ul>
+    `
+}
+
+sitemap.forEach(l=>{
+    linksSiteMap.innerHTML += populateSiteMap(l)
 })
